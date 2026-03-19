dd0b60dd0b3857ab3997cb2b521c1850860947cd
MyNameJaeff <fredrik.andersson@chasacademy.se>
2026-03-19 18:54:40 +0100

    Change name, npm build fixes

--- portfolio_updated/app/layout.tsx
+++ portfolio_updated/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Providers } from "@/components/Providers";
+import { Providers } from "@/components/PageProviders";
 import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
